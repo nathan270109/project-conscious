72f8e4f4fa3ac928e8105709c04ae3b1275de486
nathan270109 <natiel228@gmail.com>
2026-09-17 15:55:38 -0300

    feat: caminhos conectados e organizados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
+
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
--- src/app/feats/home/home.spec.ts
+++ src/app/feats/home/home.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Components } from './components';
+import { Home } from './home';
 
-describe('Components', () => {
-  let component: Components;
-  let fixture: ComponentFixture<Components>;
+describe('Home', () => {
+  let component: Home;
+  let fixture: ComponentFixture<Home>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Components],
+      imports: [Home],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Components);
+    fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
