d9c9a0c68d2b2e06f387f60b2339fc02016fbaa5
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-18 14:35:11 -0300

    cria componente secao-hero na pasta home

---
+++ src/app/feats/home/secao-hero/secao-hero.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-secao-hero',
+  styleUrl: './secao-hero.css',
+  templateUrl: './secao-hero.html',
+})
+export class SecaoHero {}
