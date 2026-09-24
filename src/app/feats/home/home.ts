888ce97c0ccd87d120486b4480c3a06327f11a2f
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-24 17:17:41 -0300

    feat: cria cinco-dimensoes

--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { SecaoHero } from './secao-hero/secao-hero';
 import { SobreOProjeto } from './sobre-o-projeto/sobre-o-projeto';
 import { ComoFunciona } from './como-funciona/como-funciona';
+import { CincoDimensoes } from './cinco-dimensoes/cinco-dimensoes';
 
 @Component({
-  imports: [SecaoHero, SobreOProjeto, ComoFunciona],
+  imports: [SecaoHero, SobreOProjeto, ComoFunciona, CincoDimensoes],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
