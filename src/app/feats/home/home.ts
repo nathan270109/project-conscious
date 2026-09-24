e909b613b54e7cfd375c0d822dc9671248973d84
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-23 22:58:23 -0300

    feat: conecta componente como-funciona na pasta home

--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecaoHero } from './secao-hero/secao-hero';
 import { SobreOProjeto } from './sobre-o-projeto/sobre-o-projeto';
+import { ComoFunciona } from './como-funciona/como-funciona';
 
 @Component({
-  imports: [SecaoHero, SobreOProjeto],
+  imports: [SecaoHero, SobreOProjeto, ComoFunciona],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
